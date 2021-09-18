633c077bd3122755405426959b21fb0c158cb064
Chuang Yu <cyu9960@gmail.com>
2021-09-18 15:56:07 +0000

    make 'render' an instance method

--- examples/sphere.rs
+++ examples/sphere.rs
@@ -1,5 +1,5 @@
 use t3::geometry::Sphere;
-use t3::glam::Vec3;
+use t3::math::Vec3;
 
 fn main() {
     let mut app = t3::App::new();
@@ -10,7 +10,7 @@ fn main() {
     app.scene.add_geometry(sphere);
     app.scene.add_light(light);
 
-    t3::render(app, move |app, time| {
+    app.render(move |app, time| {
         let lights = &mut app.scene.lights;
         let light = &mut lights[0];
         let light_pos = light.get_pos();
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub use glam;
+pub use glam as math;
 mod camera;
 pub mod canvas;
 pub mod color;
@@ -33,93 +33,104 @@ impl App {
         }
     }
 
+    pub fn render<F>(&mut self, mut f: F)
+    where
+        F: FnMut(&mut App, u128) + std::marker::Send + 'static,
+        Self: 'static,
+    {
+        let (tx, rx) = mpsc::channel();
+
+        let handler = thread::spawn(move || {
+            let stdin = stdin();
+            let keys = stdin.keys();
+            for c in keys {
+                match c.unwrap() {
+                    // Exit.
+                    Key::Char('q') => {
+                        tx.send(()).unwrap();
+                        break;
+                    }
+                    _ => (),
+                }
+            }
+        });
+
+        let timer = Instant::now();
+        loop {
+            match rx.try_recv() {
+                Ok(_) | Err(TryRecvError::Disconnected) => {
+                    // Terminating
+                    self.dispose();
+                    break;
+                }
+                Err(TryRecvError::Empty) => {}
+            }
+
+            self.canvas.clear_screen();
+
+            let (canvas_col, canvas_row) = canvas::Canvas::get_size();
+            let sphere = self.scene.geometries.first().unwrap();
+
+            for j in 1..=canvas_row {
+                for i in 1..=canvas_col {
+                    // build ray
+                    let r = ray::Ray::new(self.camera.position, {
+                        // remap x, y to (-1.0, 1.0)
+                        let (x, y) = (
+                            util::remap(
+                                (i - 1) as f32,
+                                (0.0, (canvas_col - 1) as f32),
+                                (-1.0, 1.0),
+                            ),
+                            util::remap(
+                                (j - 1) as f32,
+                                (0.0, (canvas_row - 1) as f32),
+                                (1.0, -1.0),
+                            ),
+                        );
+                        Vec3::new(x, y, -1.0).normalize()
+                    });
+
+                    if let Some(hit_point) = r.hit(sphere) {
+                        let mut color = color::Color {
+                            r: 0.0,
+                            g: 0.0,
+                            b: 0.0,
+                        };
+
+                        // ambient
+                        color += 0.5;
+
+                        // diffuse
+                        let light = self.scene.lights.first().unwrap();
+                        let light_pos = light.get_pos();
+                        let p_nor = (hit_point - sphere.origin).normalize();
+                        let angle = p_nor.dot((*light_pos - p_nor).normalize());
+                        color += if angle >= 0.0 {
+                            util::remap(angle, (0.0, 1.0), (0.0, 0.5))
+                        } else {
+                            0.0
+                        };
+
+                        // todo: specular
+                        self.canvas.write(i, j, color.get_char());
+                    } else {
+                        self.canvas.write(i, j, ' ')
+                    }
+                }
+            }
+
+            self.canvas.flush();
+
+            thread::sleep(Duration::from_millis(200));
+
+            f(self, timer.elapsed().as_millis());
+        }
+
+        handler.join().unwrap();
+    }
+
     fn dispose(&mut self) {
         self.canvas.dispose();
     }
-}
-
-pub fn render<F>(mut app: App, mut f: F)
-where
-    F: FnMut(&mut App, u128) + std::marker::Send + 'static,
-{
-    let (tx, rx) = mpsc::channel();
-
-    let timer = Instant::now();
-    let render_thread = thread::spawn(move || loop {
-        match rx.try_recv() {
-            Ok(_) | Err(TryRecvError::Disconnected) => {
-                // Terminating
-                app.dispose();
-                break;
-            }
-            Err(TryRecvError::Empty) => {}
-        }
-
-        app.canvas.clear_screen();
-
-        let (canvas_col, canvas_row) = canvas::Canvas::get_size();
-        let sphere = app.scene.geometries.first().unwrap();
-
-        for j in 1..=canvas_row {
-            for i in 1..=canvas_col {
-                // build ray
-                let r = ray::Ray::new(app.camera.position, {
-                    // remap x, y to (-1.0, 1.0)
-                    let (x, y) = (
-                        util::remap((i - 1) as f32, (0.0, (canvas_col - 1) as f32), (-1.0, 1.0)),
-                        util::remap((j - 1) as f32, (0.0, (canvas_row - 1) as f32), (1.0, -1.0)),
-                    );
-                    Vec3::new(x, y, -1.0).normalize()
-                });
-
-                if let Some(hit_point) = r.hit(sphere) {
-                    let mut color = color::Color {
-                        r: 0.0,
-                        g: 0.0,
-                        b: 0.0,
-                    };
-
-                    // ambient
-                    color += 0.5;
-
-                    // diffuse
-                    let light = app.scene.lights.first().unwrap();
-                    let light_pos = light.get_pos();
-                    let p_nor = (hit_point - sphere.origin).normalize();
-                    let angle = p_nor.dot((*light_pos - p_nor).normalize());
-                    color += if angle >= 0.0 {
-                        util::remap(angle, (0.0, 1.0), (0.0, 0.5))
-                    } else {
-                        0.0
-                    };
-
-                    // todo: specular
-                    app.canvas.write(i, j, color.get_char());
-                } else {
-                    app.canvas.write(i, j, ' ')
-                }
-            }
-        }
-
-        app.canvas.flush();
-
-        thread::sleep(Duration::from_millis(200));
-
-        f(&mut app, timer.elapsed().as_millis());
-    });
-
-    let stdin = stdin();
-    let keys = stdin.keys();
-    for c in keys {
-        match c.unwrap() {
-            // Exit.
-            Key::Char('q') => {
-                tx.send(()).unwrap();
-                break;
-            }
-            _ => (),
-        }
-    }
-
-    render_thread.join().unwrap();
 }
